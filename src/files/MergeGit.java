e0c130ddf5d2608d860e38c1d1717f9ad5ff687a
Balanand Jha <balanandjha26@gmail.com>
2020-09-18 20:22:25 +0530

    This commit is made to understand git merge

---
+++ src/files/MergeGit.java
@@ -0,0 +1,10 @@
+package files;
+
+public class MergeGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This code is written for git  merge tutorial");
+	}
+
+}
